c6b16ff4c53b063dd83a4dd796b3402c76ae0ebb
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2024-10-11 14:13:35 +0300

    third  update

---
+++ index.js
@@ -0,0 +1,7 @@
+let select =document.querySelector('.card_course')
+
+document.addEventListener("click", myFunction);
+function myFunction(){
+    
+    document.location.replace("./course.html")
+}
